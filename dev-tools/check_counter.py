b6ada8858439e554c156e966624bf2a832024aae
Mark Rudolph <mark@bishopcranmer.com>
2016-09-28 09:27:47 -0400

    Minor English Fixes

--- dev-tools/check_counter.py
+++ dev-tools/check_counter.py
@@ -1,6 +1,6 @@
 # Tool created for bug #564
 # This bug was, in reality, a LaTeX bug and not a reledmac bug
-# This tools look on a .log file, and check if the counter linked to a `insert` is not assigned a twice
+# This tool looks in a .log file, and checks if the counter linked to an `insert` is not assigned twice
 log = open("issue564.log","r")
 data = {}
 import re
