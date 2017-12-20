37316c4bdcb81ccb9df6828dda2169abb9e9c7e1
Maïeul Rouquette <maieul@maieul.net>
2017-12-21 00:21:38 +0100

    tester les bon fichiers, avec le bon numbre de run, avec les bon moteurs

--- build.lua
+++ build.lua
@@ -1,5 +1,7 @@
 #!/usr/bin/env texlua
 module="reledmac"
-testfiledir= "./examples"
+stdengine={"xetex","luatex"}
+checkengines={"xetex","luatex"}
+checkruns=2
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
