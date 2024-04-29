33530248c4cd899bd84844d54293552420030cbd
Maïeul Rouquette <maieul@maieul.net>
2024-04-29 16:20:23 +0200

    tests: check all config files

--- build.lua
+++ build.lua
@@ -1,8 +1,15 @@
 #!/usr/bin/env texlua
 module="reledmac"
 checkconfigs = {
+	"config-pdftex-1",
 	"config-pdftex-2",
 	"config-pdftex-3",
 	"config-pdftex-4",
+	"config-pdftex-5",
+	"config-luatex-2",
 	"config-luatex-3",
-	"config-xetex-4"}
+	"config-luatex-4",
+	"config-xetex-2",
+	"config-xetex-3",
+	"config-xetex-4"
+}
