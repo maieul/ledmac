fbfbc0cd77b50c6f1fdbd23f43bb215543f0c0e5
Maïeul Rouquette <maieul@maieul.net>
2024-05-10 22:19:26 +0200

    test+doc: #962

--- build.lua
+++ build.lua
@@ -4,5 +4,6 @@ checkconfigs = {
 	"config-pdftex-2",
 	"config-pdftex-3",
 	"config-pdftex-4",
+	"config-luatex-1",
 	"config-luatex-3",
 	"config-xetex-4"}
---
+++ config-luatex-1.lua
@@ -0,0 +1,4 @@
+testfiledir  = "testfiles-luatex-1"
+checkengines =  {"luatex"}
+stdengine =  "luatex"
+checkruns = 1
